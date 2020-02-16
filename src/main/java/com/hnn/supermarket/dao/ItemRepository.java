b20476a20508adaa9122dcb574eaae908808e047
hnnfr <59726038+hnnfr@users.noreply.github.com>
2020-02-16 23:22:55 +0100

    CodeKata: clean code, remove 'value' of @Query

--- src/main/java/com/hnn/supermarket/dao/ItemRepository.java
+++ src/main/java/com/hnn/supermarket/dao/ItemRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Item getItemByCode(String code);
 
-    @Query(value = "select i from Item i left join fetch i.pricingHistories where i.code = :code")
+    @Query("select i from Item i left join fetch i.pricingHistories where i.code = :code")
     Item getItemWithPricingHistories(String code);
 }
--- src/main/java/com/hnn/supermarket/dao/PricingPolicyRepository.java
+++ src/main/java/com/hnn/supermarket/dao/PricingPolicyRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface PricingPolicyRepository extends JpaRepository<PricingPolicy, Long> {
     PricingPolicy getPricingPolicyByCode(String code);
 
-    @Query(value = "select pp from PricingPolicy pp left join fetch pp.items where pp.code = :code")
+    @Query("select pp from PricingPolicy pp left join fetch pp.items where pp.code = :code")
     PricingPolicy getPricingPolicyWithItems(String code);
 }
